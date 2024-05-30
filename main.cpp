3ae687e315bf1721651c0132669843d14544e5e0
miguelsando12345 <146867633+miguelsando12345@users.noreply.github.com>
2024-05-30 09:08:22 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero1, numero2;
+
+    cout << "Ingrese el primer número: ";
+    cin >> numero1;
+
+    cout << "Ingrese el segundo número: ";
+    cin >> numero2;
+
+    int suma = numero1 + numero2;
+
+    cout << "La suma es: " << suma << endl;
+
+    return 0;
+}
