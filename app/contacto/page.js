48fe191d7dc52dc6308e427d280d94abaea10171
miguelsando12345 <146867633+miguelsando12345@users.noreply.github.com>
2024-07-02 10:42:26 -0600

    Add files via upload

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -1,9 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState, useRef } from "react";
 import Navbar from "../componente/navbar/Navbar";
 import ImageGallery from "react-image-gallery";
-import "bootstrap/dist/css/bootstrap.min.css"; // Importa Bootstrap CSS
+import Button from "react-bootstrap/Button";
+import Overlay from "react-bootstrap/Overlay";
 
+// Image data for the gallery
 const images = [
   {
     original:
@@ -25,18 +27,55 @@ const images = [
   },
 ];
 
-const page = () => {
+// Define the Example component separately
+function Example() {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
+  return (
+    <>
+      <Button variant="danger" ref={target} onClick={() => setShow(!show)}>
+        Un gran poder conlleva, una gran responsabilidad
+      </Button>
+      <Overlay target={target.current} show={show} placement="right">
+        {({
+          placement: _placement,
+          arrowProps: _arrowProps,
+          show: _show,
+          popper: _popper,
+          hasDoneInitialMeasure: _hasDoneInitialMeasure,
+          ...props
+        }) => (
+          <div
+            {...props}
+            style={{
+              position: "absolute",
+              backgroundColor: "rgba(255, 100, 100, 0.85)",
+              padding: "2px 10px",
+              color: "white",
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+            Simple tooltip
+          </div>
+        )}
+      </Overlay>
+    </>
+  );
+}
+
+// Define the main page component
+const Page = () => {
   return (
     <>
       <Navbar />
       <ImageGallery items={images} />
-      <div className="text-center my-5">
-        <h2 className="display-4 fw-bold">Un gran poder</h2>
-        <p className="lead">conlleva una gran responsabilidad</p>
-      </div>
-      <div>contacto</div>
+      <div></div>
+      {/* Render the Example component */}
+      <Example />
     </>
   );
 };
 
-export default page;
+export default Page;
